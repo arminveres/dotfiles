c49d7348b340ad814c0681fcbe5bdd9c7ab7928f
arminveres <armin.veres@hotmail.com>
2021-11-01 14:06:51 +0100

    updated files for new laptop

--- deploymentScripts/deploy-conf.sh
+++ deploymentScripts/deploy-conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ./i3conf/config ~/.config/i3/
-cp ./i3conf/i3status.conf /etc/
-cp ./.zshrc ~/
+cp ../i3conf/config ~/.config/i3/
+cp ../i3conf/i3status.conf /etc/
+cp ../.zshrc ~/
