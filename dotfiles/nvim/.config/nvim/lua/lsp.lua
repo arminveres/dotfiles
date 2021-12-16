badc3c899e0d2fd4a84f48fc2523aefd968beb14
arminveres <armin.veres@hotmail.com>
2021-12-16 15:02:11 +0100

    lsp installer fix: works now

--- dotfiles/nvim/.config/nvim/lua/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp.lua
@@ -40,6 +40,10 @@ end
 
 -- nvim-lsp-installer setup
 local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function (server)
+  local opts = {}
+  server:setup(opts)
+end)
 
 lsp_installer.settings({
     install_root_dir = ( vim.fn.stdpath"data" .. "/lsp_servers" )
