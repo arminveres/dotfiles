fe531894d055f9a7935b75f4012d63a17065615c
arminveres <armin.veres@hotmail.com>
2022-01-27 17:17:05 +0100

    changed from anaconda to miniconda

--- dotfiles/zsh/.config/zsh/exports.zsh
+++ dotfiles/zsh/.config/zsh/exports.zsh
@@ -15,15 +15,16 @@ eval "$(zoxide init zsh)"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/arminveres/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/arminveres/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/arminveres/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/arminveres/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/arminveres/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/arminveres/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/arminveres/anaconda3/bin:$PATH"
+        export PATH="/home/arminveres/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
