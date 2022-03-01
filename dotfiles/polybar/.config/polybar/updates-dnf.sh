896224d59a204f1d2052324367c0252c83aa78cf
arminveres <armin.veres@hotmail.com>
2022-03-01 16:31:47 +0100

    polybar update

--- dotfiles/polybar/.config/polybar/updates-dnf.sh
+++ dotfiles/polybar/.config/polybar/updates-dnf.sh
@@ -2,8 +2,10 @@
 
 updates=$(dnf updateinfo -q --list | wc -l)
 
-if [ "$updates" -gt 0 ]; then
-    echo " $updates"
-else
-    echo ""
-fi
+echo " $updates"
+
+# if [ "$updates" -gt 0 ]; then
+#     echo " $updates"
+# else
+#     echo ""
+# fi
