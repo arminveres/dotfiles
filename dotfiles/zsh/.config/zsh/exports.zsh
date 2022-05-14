849cf8afaa97b90f25fd7b0dc9a9bbc206cfc674
arminveres <armin.veres@hotmail.com>
2022-05-14 15:32:01 +0200

    Cleaned up $HOME with some exports for the future

--- dotfiles/zsh/.config/zsh/exports.zsh
+++ dotfiles/zsh/.config/zsh/exports.zsh
@@ -8,7 +8,22 @@ setopt HIST_IGNORE_DUPS       # ignore duplicate commands
 setopt HIST_IGNORE_SPACE      # ignore commands starting with space
 setopt HIST_VERIFY            # show command with history expansion to user before running it
 
-export PATH=$HOME/bin:$HOME/.bin:$HOME/.local/bin:/usr/local/bin:$PATH
+# setting some XDGs
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_STATE_HOME=$HOME/.local/state
+export XDG_CACHE_HOME=$HOME/.cache
+
+export WINEPREFIX="$XDG_DATA_HOME"/wine
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export PSQL_HISTORY="$XDG_DATA_HOME/psql_history"
+export PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass"
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME"/pass
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+
+export PATH=$HOME/bin:$HOME/.bin:$HOME/.local/bin:$HOME/go/bin/:/usr/local/bin:$PATH
 export JDTLS_HOME="/home/arminveres/.local/share/nvim/lsp_servers/jdtls/"
 export VISUAL=nvim
 export EDITOR=nvim
