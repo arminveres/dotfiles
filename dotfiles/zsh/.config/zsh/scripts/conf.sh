4ac3eb6c2c792480f0dd5460ee86e706b46e66f5
arminveres <armin.veres@hotmail.com>
2022-05-28 11:35:51 +0200

    updated workstation@home: i3, zsh, picom, polybar

--- dotfiles/zsh/.config/zsh/scripts/conf.sh
+++ dotfiles/zsh/.config/zsh/scripts/conf.sh
@@ -2,10 +2,11 @@
 
 # reads in a config name
 config=`cat $HOME/.config/zsh/scripts/config_locations | fzf`
+# TODO: add check of config location
+# loc=$XDG_CONFIG_HOME/$config
 
 # NOTE: opens config folder instead of the config itself
 # TODO: open first file directly
-
 if [[ -n $config ]]; then
     nvim ~/.config/$config
 else
