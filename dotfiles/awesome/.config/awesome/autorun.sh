d8b42d337474f4675a60b2253496bb98a47f6399
Armin Veres <armin.veres@hotmail.com>
2022-11-01 16:37:08 +0100

    [fonts] Added tamzen font

--- dotfiles/awesome/.config/awesome/autorun.sh
+++ dotfiles/awesome/.config/awesome/autorun.sh
@@ -2,22 +2,19 @@
 
 # run "program [some arguments]"
 run() {
-  if ! pgrep -f "$1" ;
-  then
-    "$@"&
-  fi
+	if ! pgrep -f "$1"; then
+		"$@" &
+	fi
 }
 
 setxkbmap de us
 export $(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg)
 gnome-keyring-daemon --daemonize --login
 
-run nitrogen --restore
 run /usr/libexec/polkit-gnome-authentication-agent-1
 run picom --daemon --experimental-backend --config ~/.config/picom/picom.conf
 run udiskie --tray --notify
 run blueman-manager
-run corectrl
 run nm-applet
 run pasystray
 run xrandr --output DisplayPort-1 --primary --mode 3440x1440 --rate 144
@@ -27,9 +24,13 @@ run xinput --set-prop 'Logitech G Pro' 'libinput Accel Profile Enabled' 0, 1
 run xinput --set-prop 'pointer:Logitech G305' 'libinput Accel Profile Enabled' 0, 1
 
 # Laptop/Notebook specific settings
-if [[ ! -z `uname --nodename | grep notebook` && -z `xinput | grep M60` ]]; then
-  setxkbmap -option 'ctrl:swapcaps,altwin:swap_alt_win'
-  xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Tapping Enabled' 1
-  xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Natural Scrolling Enabled' 1
-  run xss-lock --transfer-sleep-lock -- i3lock-blur --nofork # locks screen when clising the lid
+if [[ ! -z $(uname --nodename | grep notebook) && -z $(xinput | grep M60) ]]; then
+	setxkbmap -option 'ctrl:swapcaps,altwin:swap_alt_win'
+	xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Tapping Enabled' 1
+	xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Natural Scrolling Enabled' 1
+	run xss-lock --transfer-sleep-lock -- i3lock-blur --nofork # locks screen when clising the lid
+else
+	run corectrl
 fi
+
+run nitrogen --restore
