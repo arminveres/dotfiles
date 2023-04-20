ffe8536c6d69e006d1920a059b405607019ce3ac
Armin Veres <armin.veres@hotmail.com>
2023-04-20 18:31:55 +0200

    refactor(awesomewm): moved rules into separate file

---
+++ awesome/.config/awesome/myrules.lua
@@ -0,0 +1,107 @@
+local awful = require('awful')
+local beautiful = require('beautiful')
+local clientkeys = require('mappings').clientkeys
+
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    {
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            -- sets the slave as the new window
+            -- callback = awful.client.setslave,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = clientkeys,
+            buttons = clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+            size_hints_honor = false,
+        },
+    },
+
+    -- Floating clients.
+    {
+        rule_any = {
+            instance = {
+                'DTA',   -- Firefox addon DownThemAll.
+                'copyq', -- Includes session name in class.
+                'pinentry',
+            },
+            class = {
+                'Arandr',
+                'gnome-calculator',
+                'nm-connection-editor',
+                'Gpick',
+                'Kruler',
+                'MessageWin',  -- kalarm.
+                'Sxiv',
+                'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
+                'Wpa_gui',
+                'veromix',
+                'xtightvncviewer',
+                'origin.exe',
+                'zoom',
+                'Cisco AnyConnect Secure Mobility Client',
+                'Matplotlib',
+                'matplotlib'
+            },
+            -- Note that the name property shown in xprop might be set slightly after creation of the client
+            -- and the name shown there might not match defined rules here.
+            name = {
+                'Event Tester', -- xev.
+            },
+            role = {
+                'AlarmWindow',   -- Thunderbird's calendar.
+                'ConfigManager', -- Thunderbird's about:config.
+                'pop-up',        -- e.g. Google Chrome's (detached) Developer Tools.
+                'Network Connections',
+            },
+        },
+        properties = {
+            floating = true,
+        },
+    },
+
+    -- Add titlebars to normal clients and dialogs
+    {
+        rule_any = { type = { 'normal', 'dialog' } },
+        properties = { titlebars_enabled = false },
+    },
+
+    -- Set Firefox to always map on the tag named "2" on screen 1.
+    {
+        rule = { class = 'Firefox' },
+        properties = { screen = 1, tag = awful.util.tagnames[2] },
+    },
+    {
+        rule_any = { class = { 'Blueman-manager', 'easyeffects' } },
+        properties = { tag = awful.util.tagnames[4], minimized = true },
+    },
+    {
+        rule_any = { class = { 'Spotify' } },
+        properties = { tag = awful.util.tagnames[4] },
+    },
+    {
+        rule_any = { class = { 'Geary', 'thunderbird', 'mail' } },
+        properties = { tag = awful.util.tagnames[5] },
+    },
+    {
+        rule_any = { class = { 'Steam', 'Lutris' } },
+        properties = { tag = awful.util.tagnames[6] },
+    },
+    {
+        rule_any = { class = { 'qBittorrent', 'qbittorrent' } },
+        properties = { tag = awful.util.tagnames[7] },
+    },
+    {
+        rule_any = { class = { 'corectrl', 'virt-manager', 'Virt-manager' } },
+        properties = { tag = awful.util.tagnames[8] },
+    },
+    {
+        rule_any = { class = { 'discord', 'Signal', 'Slack' } },
+        properties = { tag = awful.util.tagnames[9] },
+    },
+}
--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -4,8 +4,7 @@
      github.com/lcpz
 
 --]]
-
--- {{{ Required libraries
+-- Required libraries
 
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
@@ -27,10 +26,7 @@ local lain = require('lain')
 local mappings = require('mappings')
 local mytable = mappings.mytable
 local globalkeys = mappings.globalkeys
-local clientkeys = mappings.clientkeys
 local dpi = require('beautiful.xresources').apply_dpi
--- }}}
-local tag = tag
 
 -- Notification configuration
 naughty.config.defaults.border_width = dpi(2)
@@ -76,7 +72,7 @@ awful.spawn.with_shell('~/.config/awesome/autorun.sh')
 -- {{{ Variable definitions
 
 local themes = {
-    'default', -- 1
+    'default',    -- 1
     'multicolor', -- 2
 }
 
@@ -88,9 +84,6 @@ local editor = os.getenv('EDITOR') or 'nvim'
 
 awful.util.terminal = terminal
 awful.util.tagnames = { 'code', 'www', 'res', 'mus', 'mail', 'game', 'tor', 'vm', 'msg' }
-
--- TODO: set mw factor acording to tag and screensize
--- awful.tag.setmwfact(0.7, null)
 
 awful.layout.layouts = {
     lain.layout.centerwork,
@@ -188,9 +181,9 @@ local myawesomemenu = {
             hotkeys_popup.show_help(nil, awful.screen.focused())
         end,
     },
-    { 'Manual', string.format('%s -e man awesome', terminal) },
+    { 'Manual',      string.format('%s -e man awesome', terminal) },
     { 'Edit config', string.format('%s -e %s %s', terminal, editor, awesome.conffile) },
-    { 'Restart', awesome.restart },
+    { 'Restart',     awesome.restart },
     {
         'Quit',
         function()
@@ -215,8 +208,8 @@ awful.util.mymainmenu.wibox:connect_signal('mouse::leave', function()
     if
         not awful.util.mymainmenu.active_child
         or (
-            awful.util.mymainmenu.wibox ~= mouse.current_wibox
-            and awful.util.mymainmenu.active_child.wibox ~= mouse.current_wibox
+        awful.util.mymainmenu.wibox ~= mouse.current_wibox
+        and awful.util.mymainmenu.active_child.wibox ~= mouse.current_wibox
         )
     then
         awful.util.mymainmenu:hide()
@@ -255,24 +248,26 @@ end)
 
 -- Create a wibox for each screen and add it
 awful.screen.connect_for_each_screen(function(s)
+    -- if s.geometry.width > 1920 then
+    --     s.master_width_factor = 0.7
+    -- end
     require('bars').at_screen_connect(s)
 end)
 
 -- }}}
 
 bling.widget.window_switcher.enable({
-    type = 'thumbnail', -- set to anything other than "thumbnail" to disable client previews
+    type = 'thumbnail',                           -- set to anything other than "thumbnail" to disable client previews
     -- keybindings (the examples provided are also the default if kept unset)
-    hide_window_switcher_key = 'Escape', -- The key on which to close the popup
-    minimize_key = 'n', -- The key on which to minimize the selected client
-    unminimize_key = 'N', -- The key on which to unminimize all clients
-    kill_client_key = 'q', -- The key on which to close the selected client
-    cycle_key = 'Tab', -- The key on which to cycle through all clients
-    previous_key = 'Left', -- The key on which to select the previous client
-    next_key = 'Right', -- The key on which to select the next client
-    vim_previous_key = 'l', -- Alternative key on which to select the previous client
-    vim_next_key = 'h', -- Alternative key on which to select the next client
-
+    hide_window_switcher_key = 'Escape',          -- The key on which to close the popup
+    minimize_key = 'n',                           -- The key on which to minimize the selected client
+    unminimize_key = 'N',                         -- The key on which to unminimize all clients
+    kill_client_key = 'q',                        -- The key on which to close the selected client
+    cycle_key = 'Tab',                            -- The key on which to cycle through all clients
+    previous_key = 'Left',                        -- The key on which to select the previous client
+    next_key = 'Right',                           -- The key on which to select the next client
+    vim_previous_key = 'l',                       -- Alternative key on which to select the previous client
+    vim_next_key = 'h',                           -- Alternative key on which to select the next client
     -- filterClients = awful.widget.tasklist.filter.currenttags, -- The function to filter the viewed clients
     cycleClientsByIdx = awful.client.focus.byidx, -- The function to cycle the clients
     -- filterClients = awful.widget.tasklist.filter.allscreen
@@ -283,9 +278,9 @@ root.buttons(mytable.join(
     awful.button({}, 3, function()
         awful.util.mymainmenu:toggle()
     end)
-    -- NOTE: This got quite annoying when accidentally scrolling on the desktop.
-    --     awful.button({}, 4, awful.tag.viewnext),
-    --     awful.button({}, 5, awful.tag.viewprev)
+-- NOTE: This got quite annoying when accidentally scrolling on the desktop.
+--     awful.button({}, 4, awful.tag.viewnext),
+--     awful.button({}, 5, awful.tag.viewprev)
 ))
 
 -- }}}
@@ -355,7 +350,7 @@ clientbuttons = mytable.join(
         -- Only use bottom left/right corner, because dragging titlebar is already mapped to move
         local corners = {
             { c.x + c.width, c.y + c.height },
-            { c.x, c.y + c.height },
+            { c.x,           c.y + c.height },
         }
         local m = mouse.coords()
         local distance = 20
@@ -371,116 +366,13 @@ clientbuttons = mytable.join(
 -- Set keys
 root.keys(globalkeys)
 
--- {{{ Rules
--- TODO: move into separate file
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-    -- All clients will match this rule.
-    {
-        rule = {},
-        properties = {
-            border_width = beautiful.border_width,
-            border_color = beautiful.border_normal,
-            -- sets the slave as the new window
-            -- callback = awful.client.setslave,
-            focus = awful.client.focus.filter,
-            raise = true,
-            keys = clientkeys,
-            buttons = clientbuttons,
-            screen = awful.screen.preferred,
-            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-            size_hints_honor = false,
-        },
-    },
-
-    -- Floating clients.
-    {
-        rule_any = {
-            instance = {
-                'DTA', -- Firefox addon DownThemAll.
-                'copyq', -- Includes session name in class.
-                'pinentry',
-            },
-            class = {
-                'Arandr',
-                'gnome-calculator',
-                'nm-connection-editor',
-                'Gpick',
-                'Kruler',
-                'MessageWin', -- kalarm.
-                'Sxiv',
-                'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
-                'Wpa_gui',
-                'veromix',
-                'xtightvncviewer',
-                'origin.exe',
-                'zoom',
-                'Cisco AnyConnect Secure Mobility Client',
-                'Matplotlib',
-                'matplotlib'
-            },
-
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name = {
-                'Event Tester', -- xev.
-            },
-            role = {
-                'AlarmWindow', -- Thunderbird's calendar.
-                'ConfigManager', -- Thunderbird's about:config.
-                'pop-up', -- e.g. Google Chrome's (detached) Developer Tools.
-                'Network Connections',
-            },
-        },
-        properties = {
-            floating = true,
-        },
-    },
-
-    -- Add titlebars to normal clients and dialogs
-    {
-        rule_any = { type = { 'normal', 'dialog' } },
-        properties = { titlebars_enabled = false },
-    },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    {
-        rule = { class = 'Firefox' },
-        properties = { screen = 1, tag = awful.util.tagnames[2] },
-    },
-    {
-        rule_any = { class = { 'Blueman-manager', 'easyeffects' } },
-        properties = { tag = awful.util.tagnames[4], minimized = true },
-    },
-    {
-        rule_any = { class = { 'Geary', 'thunderbird', 'mail' } },
-        properties = { tag = awful.util.tagnames[5] },
-    },
-    {
-        rule_any = { class = { 'Steam', 'Lutris' } },
-        properties = { tag = awful.util.tagnames[6] },
-    },
-    {
-        rule_any = { class = { 'qBittorrent', 'qbittorrent' } },
-        properties = { tag = awful.util.tagnames[7] },
-    },
-    {
-        rule_any = { class = { 'corectrl', 'virt-manager', 'Virt-manager' } },
-        properties = { tag = awful.util.tagnames[8] },
-    },
-    {
-        rule_any = { class = { 'discord', 'Signal', 'Slack' } },
-        properties = { tag = awful.util.tagnames[9] },
-    },
-}
-
--- }}}
-
 -- {{{ Signals
 
 -- @brief adjusts the master_width_factor if we are using the lain layout, quite useful for ultrawide monitors
 local function lain_adjust_mwfact()
+    -- TODO: (aver) Add check for floating clients, so they do not affect mwfact
     local scr = awful.screen.focused()
+    -- local only_one = #scr.tiled_clients == 1
 
     if awful.layout.get(scr) == lain.layout.centerwork then
         if #scr.clients == 1 and scr.geometry.width > 1920 then
@@ -488,6 +380,8 @@ local function lain_adjust_mwfact()
         else
             scr.selected_tag.master_width_factor = 0.5
         end
+    elseif scr.selected_tag ~= nil then
+        scr.selected_tag.master_width_factor = 0.5
     end
 end
 
@@ -524,20 +418,24 @@ client.connect_signal('request::titlebars', function(c)
     )
 
     awful.titlebar(c, { size = 16 }):setup({
-        { -- Left
+        {
+            -- Left
             awful.titlebar.widget.iconwidget(c),
             buttons = buttons,
             layout = wibox.layout.fixed.horizontal,
         },
-        { -- Middle
-            { -- Title
+        {
+            -- Middle
+            {
+                -- Title
                 align = 'center',
                 widget = awful.titlebar.widget.titlewidget(c),
             },
             buttons = buttons,
             layout = wibox.layout.flex.horizontal,
         },
-        { -- Right
+        {
+            -- Right
             awful.titlebar.widget.floatingbutton(c),
             awful.titlebar.widget.maximizedbutton(c),
             awful.titlebar.widget.stickybutton(c),
@@ -574,4 +472,8 @@ client.connect_signal('unfocus', function(c)
     -- end
 end)
 
+-- }}}
+
+-- {{{ Rules
+require('myrules')
 -- }}}
