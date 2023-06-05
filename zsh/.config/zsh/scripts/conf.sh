0ab587ee2d19daa35b7b71e1a98dfbdcea944ed7
Armin Veres <armin.veres@hotmail.com>
2023-06-05 15:45:29 +0200

    feat: debian / fedora fd cmd

--- zsh/.config/zsh/scripts/conf.sh
+++ zsh/.config/zsh/scripts/conf.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-config=$(fd --max-depth 1 . "$XDG_CONFIG_HOME" | fzf)
+case "$(lsb_release -i | awk '{print $3}')" in
+    Fedora) # echo Ubuntu
+        fdcmd=fd
+        ;;
+    Debian) # echo Ubuntu
+        fdcmd=fdfind
+        ;;
+esac
+
+config=$($fdcmd --max-depth 1 . "$XDG_CONFIG_HOME" | fzf)
 
 if [[ -n $config ]]; then
     nvim -c "cd $config/" -c 'Telescope find_files'
