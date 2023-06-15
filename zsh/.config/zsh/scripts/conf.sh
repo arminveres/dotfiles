a16c2b348bfa2783bda9b0bb574b1cc8dc78a9ad
Armin Veres <armin.veres@hotmail.com>
2023-06-15 16:06:11 +0200

    fix(zsh): conf script extend Debian by Ubuntu

--- zsh/.config/zsh/scripts/conf.sh
+++ zsh/.config/zsh/scripts/conf.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 case "$(lsb_release -i | awk '{print $3}')" in
-    Fedora) # echo Ubuntu
-        fdcmd=fd
-        ;;
-    Debian) # echo Ubuntu
-        fdcmd=fdfind
-        ;;
+Fedora) # echo Ubuntu
+	fdcmd=fd
+	;;
+Debian | Ubuntu) # echo Ubuntu
+	fdcmd=fdfind
+	;;
 esac
 
-config=$($fdcmd --max-depth 1 . "$XDG_CONFIG_HOME" | fzf)
+# capture files in variable
+list_of_configs=$(
+	"$fdcmd" --max-depth 1 . "$XDG_CONFIG_HOME" | fzf
+)
 
-if [[ -n $config ]]; then
-    nvim -c "cd $config/" -c 'Telescope find_files'
+if [[ -n $list_of_configs ]]; then
+	nvim -c "cd $list_of_configs/" -c 'Telescope find_files'
 else
-    printf "Nothing selected!\n"
+	printf "Nothing selected!\n"
 fi
