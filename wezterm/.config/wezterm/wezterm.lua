f15b1b477b865eaf7df608781979b29b0066cb0c
Armin Veres <armin.veres@hotmail.com>
2023-06-22 18:19:39 +0200

    fix(wezterm): tamzen underline

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,9 +9,6 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     color_scheme = "Gruvbox dark, hard (base16)",
     window_background_opacity = 0.95,
-    -- You can specify some parameters to influence the font selection;
-    -- for example, this selects a Bold, Italic font variant.
-    -- font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true }),
 
     font = wezterm.font_with_fallback {
         {
@@ -21,7 +18,16 @@ return {
             harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
         },
     },
+    underline_position = "-3", -- fix underline for tamzen, otherwise too high
     font_size = 8,
 
     -- font = wezterm.font 'Terminus',
+    -- font = wezterm.font('DinaRemasterII'),
+    -- font_size = 12.0,
+    -- font = wezterm.font({ family = 'Hack Nerd Font Mono' }),
+    -- font = wezterm.font({ family = 'RobotoMono Nerd Font Mono' }),
+    -- font = wezterm.font({ family = 'NotoSansMono Nerd Font Mono' }),
+    -- font_size = 10.0,
+    -- font = wezterm.font({ family = 'IosevkaTerm Nerd Font' }),
+    -- font_size = 11,
 }
