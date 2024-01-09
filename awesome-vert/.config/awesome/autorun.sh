26d8d2f14b251ff61d1b17f8b743cc77986b4b01
Armin Veres <armin.veres@hotmail.com>
2024-01-09 19:25:47 +0100

    fix: xss-lock for workstation as well

--- awesome-vert/.config/awesome/autorun.sh
+++ awesome-vert/.config/awesome/autorun.sh
@@ -23,6 +23,8 @@ run xinput --set-prop 'Logitech MX Master 3' 'libinput Accel Profile Enabled' 0,
 run xinput --set-prop 'Logitech G Pro' 'libinput Accel Profile Enabled' 0, 1
 run xinput --set-prop 'pointer:Logitech G305' 'libinput Accel Profile Enabled' 0, 1
 
+run xss-lock --transfer-sleep-lock -- i3lock-blur --nofork # locks screen when closing the lid
+
 # Laptop/Notebook specific settings
 if uname --nodename | grep -q notebook; then
 	# WARN: (aver) this block needs to first on the laptop
@@ -33,7 +35,6 @@ if uname --nodename | grep -q notebook; then
 
 	xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Tapping Enabled' 1
 	xinput set-prop 'ELAN0672:00 04F3:3187 Touchpad' 'libinput Natural Scrolling Enabled' 1
-	run xss-lock --transfer-sleep-lock -- i3lock-blur --nofork # locks screen when closing the lid
 	if ! xinput | grep -q M60 && ! xinput | grep -q Sofle; then
 		# only swap ctrl and caps lock, if we are not connected to already pre-swapped keyboards
 		# setxkbmap -option 'ctrl:swapcaps,altwin:swap_alt_win'
