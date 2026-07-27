0e4273597fdba2037af19409d0cc41ab322e67b4
Armin Richard Veres <armin.veres@proton.me>
2026-07-27 14:00:28 +0200

    feat(tmux): disable agent pluging autostart

--- tmux/.config/tmux/.nvim.lua
+++ tmux/.config/tmux/.nvim.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = { "tmux.conf", ".tmux.conf" },
-  callback = function()
-    vim.lsp.start({
-      name = "tmux",
-      cmd = { "tmux-language-server" }
-    })
-  end,
+	pattern = { "tmux.conf", ".tmux.conf", "tmux.remote.conf" },
+	callback = function()
+		vim.lsp.start({
+			name = "tmux",
+			cmd = { "tmux-language-server" },
+		})
+	end,
 })
