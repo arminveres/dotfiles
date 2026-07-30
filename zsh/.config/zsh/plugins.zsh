5315fcfb00e7e03d1eb950d27f433afddce2874d
Armin Richard Veres <armin.veres@proton.me>
2026-07-30 10:51:00 +0200

    fix(zsh): windows and zvm

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -5,6 +5,13 @@
 #
 # NOTE(aver): order matters, zsh-syntax-highlighting must be sourced last.
 
+# On MSYS2/Cygwin, TERM is typically `cygwin`, which makes zvm_cursor_style fall
+# into its default branch and evaluate `=~ '\e\][0-9]+;.+\a'`. That regex is
+# rejected by the MSYS regex engine ("failed to compile regex: trailing
+# backslash"). Pinning ZVM_TERM to an xterm-compatible value avoids that branch.
+if [[ $OSTYPE == (msys|cygwin)* ]]; then
+  ZVM_TERM=${ZVM_TERM:-xterm-256color}
+fi
 source "$ZDOTDIR/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 source "$ZDOTDIR/plugins/git-prompt.zsh/git-prompt.zsh"
 # autopair.zsh self-initializes at the end of the file, no autopair-init needed.
